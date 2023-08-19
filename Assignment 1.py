f7265d005158f9b7c0d87f7ea62fc739b37992fd
Ramesh Kumar <135141211+Rameshkumar2023@users.noreply.github.com>
2023-08-19 13:46:53 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# Q1. Create one variable containing following type of data:
+#(i) string
+#(ii) list
+#(iii) float
+#(iv) tuple
+
+
+# In[10]:
+
+
+# (i) string 
+
+x = ("My name is Ramesh Kumar")
+print(x)
+
+
+# In[19]:
+
+
+# (ii) list 
+l = [1,2,3,4]
+print(l)
+
+
+# In[12]:
+
+
+# (iii) float 
+f= 3.5
+print(f)
+
+
+# In[18]:
+
+
+# (iV) tuple 
+
+t=(1,2,3,4,5)
+print(1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
